340569f019fd9cd9dff8a42ee09c52f0731ef456
Yogesh Kamboj <mr.kambozz@gmail.com>
2022-08-07 14:39:51 +0530

    Input:in program;Process:Addition;Output:of LT

---
+++ C/Basics/002.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,c;
+
+	a = 5;
+	b = 3;
+
+	c = a + b;
+
+	printf("%d + %d = %d",a,b,c);
+
+	return 0;
+}
+
---
+++ Java/Basics/002.java
@@ -0,0 +1,13 @@
+class Main
+{
+	public static void main(String []argv){
+		int a,b,c;
+
+		a = 5;
+		b = 3;
+
+		c = a + b;
+
+		System.out.println(a+" + "+b+" = "+c);
+	}
+}
